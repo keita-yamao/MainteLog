fde2d725cad6e2a79d86e5ff378e172fc8fbc451
keita-yamao <s3991t1222@outlook.jp>
2026-04-13 23:27:20 +0000

    first commit

---
+++ maintelog/src/test/java/io/github/keita_yamao/maintelog/MaintelogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.keita_yamao.maintelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
